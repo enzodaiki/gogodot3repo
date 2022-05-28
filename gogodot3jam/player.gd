1264391cb157aca55ed60169fe69406877b7ded1
enzodaiki <79280320+enzodaiki@users.noreply.github.com>
2022-05-28 17:42:27 -0300

    Add files via upload

---
+++ gogodot3jam/player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+const ACCELERATION = 1300
+const MAX_SPEED = 170
+const FRICTION = 10000
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		$AnimationPlayer.play("running")
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		$AnimationPlayer.play("idle")
+	
+	velocity = move_and_slide(velocity)
+
