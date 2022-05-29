6afc2e05ebc739e40080074c76ef0ae475f56a26
MrVolatileline879 <99370880+MrVolatileline879@users.noreply.github.com>
2022-05-29 11:55:28 -0300

    Add files via upload
    29/05/2022, /gogodot3assets/fire_animation_png.png, /gogodot3jam/tree_trunk_sprite2.png.piskel, /gogodot3jam/KinematicBody2D.gd

--- gogodot3jam/player.gd
+++ gogodot3jam/player.gd
@@ -7,17 +7,22 @@ const FRICTION = 10000
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	input_vector = input_vector.normalized()
+	var input_vector1 = Vector2.ZERO
+	var input_vector2 = Vector2.ZERO
+	input_vector1.x = int(Input.get_action_strength("right")) - int(Input.get_action_strength("left"))
+	input_vector1.y = int(Input.get_action_strength("down")) - int(Input.get_action_strength("up"))
+	input_vector2 = input_vector1.normalized()
 	
-	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	if input_vector2 != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector2 * MAX_SPEED, ACCELERATION * delta)
 		$AnimationPlayer.play("running")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		$AnimationPlayer.play("idle")
+		
+	if input_vector1.x != 0:
+		$Sprite.scale.x = input_vector1.x *-1
 	
 	velocity = move_and_slide(velocity)
+
 
